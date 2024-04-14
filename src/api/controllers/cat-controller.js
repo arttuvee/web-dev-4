b39dce888e969d0ace531bee83bf4b9690215b49
arttuvee <arttuvo@metropolia.fi>
2024-04-14 22:53:25 +0300

    Assignment 3 done

--- src/api/controllers/cat-controller.js
+++ src/api/controllers/cat-controller.js
@@ -13,11 +13,12 @@ const getCatById = (req, res) => {
   }
 };
 
-const postCat = (req, res) => {
-  const result = addCat(req.body);
+const postCat = async (req, res) => {
+  console.log('postCat')
+  const result = await addCat(req.body, req.file);
   if (result.cat_id) {
     res.status(201);
-    res.json({ message: "New cat added.", result });
+    res.json({message: 'New cat added.', result});
   } else {
     res.sendStatus(400);
   }
